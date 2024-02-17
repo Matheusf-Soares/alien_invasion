ea30e363834c88773d4434e8c9b98dff111ce77c
Matheusf-Soares <matheusfps27@icloud.com>
2024-02-16 21:56:37 -0300

    Adicionados movimentos laterais para a nave

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,35 @@ class AlienInvasion:
         """Inicia o loop principal do jogo"""
         while True:
             # Observa eventos de teclado e mouse
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
+            self.ship.update()
             # Redesenha a tela durante cada passagem pelo loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Deixa a tela mais recente desenhada visível
-            pygame.display.flip()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Responde as teclas pressionadas e a eventos de mouse"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Deixa a tela mais recente desenhada visível
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Cria uma instância do jogo e a executa
--- ship.py
+++ ship.py
@@ -15,6 +15,17 @@ class Ship:
         # Começa cada espaçonave nova no centro inferior da tela
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Flag de movimento -> Começa com uma nave que não está se movendo
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Atualiza a posição da nave com base na flag de movimento"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Desenha a espaçonave em sua localização atual"""
         self.screen.blit(self.image, self.rect)
