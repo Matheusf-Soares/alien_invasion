86e8e8913ba20d8efd38afb6f5399fe01b02bea9
Matheusf-Soares <matheusfps27@icloud.com>
2024-02-17 18:17:19 -0300

    Alteração de tela para fullscreen e limitação do alcance da nave

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicializa a espaçonave e define sua posição inicial"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Sobe a imagem da espaçonave e obtém seu rect
@@ -15,16 +16,22 @@ class Ship:
         # Começa cada espaçonave nova no centro inferior da tela
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Armazena um float para a posição horizontal exata da espaçonave
+        self.x = float(self.rect.x)
+
         # Flag de movimento -> Começa com uma nave que não está se movendo
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Atualiza a posição da nave com base na flag de movimento"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Atualiza o objeto rect de self.rect.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Desenha a espaçonave em sua localização atual"""
